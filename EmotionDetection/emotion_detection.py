2c57531576eaeed007b86707a93e95387f21f176
Hubert Kukla <kukla.hubert05@gmail.com>
2026-07-24 09:16:24 -0400

    finished project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,6 +4,18 @@ def emotion_detector(text_to_analyze):
     Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     Input = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, json = Input, headers = Headers)
+
+    if response.status_code == 400:
+            return {
+                "anger": None,
+                "disgust": None,
+                "fear": None,
+                "joy": None,
+                "sadness": None,
+                "dominant_emotion": None
+            }
+
+
     formatted = json.loads(response.text)
     emotions = formatted['emotionPredictions'][0]['emotion']
 
