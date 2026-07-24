8f0d2166caf0577281bd81a8f2f6ab7fcab4d11e
Hubert Kukla <kukla.hubert05@gmail.com>
2026-07-24 08:15:43 -0400

    created package

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,7 +1,18 @@
-import requests
+import requests,json
 def emotion_detector(text_to_analyze):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     Input = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, json = Input, headers = Headers)
-    return response.text
+    formatted = json.loads(response.text)
+    emotions = formatted['emotionPredictions'][0]['emotion']
+
+    maxi = 0
+    dominant = None
+    for emotion in emotions:
+        if emotions[emotion] > maxi:
+            maxi = emotions[emotion]
+            dominant = emotion
+    emotions['dominant_emotion'] = dominant
+
+    return emotions
